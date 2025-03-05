a604800e1263127b0933a12c5867a8826645efdc
samir jadhav <samirzjadhav@gmail.com>
2025-03-05 13:19:19 +0200

    feat: added list section with option

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
+
+import useStyles from "./styles";
+import { use } from "react";
 
 const List = () => {
-  return (
-    <div>List</div>
-  )
-}
+  const classes = useStyles();
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
 
-export default List
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4">
+        Restaurants, Hotels & Attractions around you
+        <FormControl className={classes.formControl}>
+          <InputLabel>Type</InputLabel>
+          <Select value={type} onChange={(e) => setType(e.target.value)}>
+            <MenuItem value="restaurants">Restaurants</MenuItem>
+            <MenuItem value="hotels">Hotels</MenuItem>
+            <MenuItem value="attractions">Attractions</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel>Rating</InputLabel>
+          <Select value={rating} onChange={(e) => setType(e.target.value)}>
+            <MenuItem value={0}>All</MenuItem>
+            <MenuItem value={3}>Above 3.0</MenuItem>
+            <MenuItem value={4}>Above 4</MenuItem>
+            <MenuItem value={4.5}>Above 4.5</MenuItem>
+          </Select>
+        </FormControl>
+      </Typography>
+    </div>
+  );
+};
+
+export default List;

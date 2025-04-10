9eb0df39dec7d4306e159ef986d3c5699f76a987
samir jadhav <samirzjadhav@gmail.com>
2025-04-10 14:25:41 +0530

    feat: added new api for check a current weather

--- src/api/index.js
+++ src/api/index.js
@@ -29,3 +29,25 @@ export const getPlacesData = async (type, sw, ne) => {
     console.log(error);
   }
 };
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    if (lat && lng) {
+      const { data } = await axios.get(
+        "https://community-open-weather-map.p.rapidapi.com/find",
+        {
+          params: { lat, lon: lng },
+          headers: {
+            "x-rapidapi-key":
+              "1b6bb72eacmsh15ba37053b7149fp1ee49bjsn2ce45c3f8e77",
+            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+          },
+        }
+      );
+
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
